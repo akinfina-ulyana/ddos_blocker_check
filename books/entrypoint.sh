d936115f35c9a0bb6bd88a1bd1398eef2203a577
uly <ulyana.akinfina22@gmail.com>
2024-09-13 18:21:32 +0300

    Add requirements.txt

---
+++ books/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'ddos')" | python manage.py shell || true
+
+python manage.py loaddata books
+
+exec "$@"
