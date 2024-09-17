f6e1eae4d3e3e166500eafb28a34b53ecb9a3d4d
uly <ulyana.akinfina22@gmail.com>
2024-09-17 15:00:25 +0300

    Change BookCreateView

--- books/api/views.py
+++ books/api/views.py
@@ -13,6 +13,13 @@ class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
@@ -22,9 +29,13 @@ class BookDetailView(generics.RetrieveAPIView):
 class BookPriceView(generics.GenericAPIView):
     serializer_class = BooksSerializer
 
+    def get_queryset(self):
+        return Book.objects.all()
+
     def get(self, request, name):
         try:
             book = Book.objects.get(name=name)
             return Response({'price': book.price}, status=status.HTTP_200_OK)
         except Book.DoesNotExist:
             return Response({'error': 'Book not found'}, status=status.HTTP_404_NOT_FOUND)
+
