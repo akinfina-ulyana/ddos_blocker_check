e538ec64b2cc5e578d2806f702d8ddc743dfea03
uly <ulyana.akinfina22@gmail.com>
2024-10-15 23:47:25 +0300

    Black editing

--- books/api/urls.py
+++ books/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from .views import BookAPIView, BookCreateView, BookDetailView, BookPriceView
 
 urlpatterns = [
-    path('books/', BookAPIView.as_view(), name='book-list'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/price/<str:name>/', BookPriceView.as_view(), name='book-price'),
+    path("books/", BookAPIView.as_view(), name="book-list"),
+    path("books/create/", BookCreateView.as_view(), name="book-create"),
+    path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
+    path("books/price/<str:name>/", BookPriceView.as_view(), name="book-price"),
 ]
