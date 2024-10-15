8c99914091a56e33627e09b0bbd4b3b54770eeec
uly <ulyana.akinfina22@gmail.com>
2024-10-15 23:47:17 +0300

    Black editing

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -1,18 +1,17 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from store.serializers import BooksSerializer
+
 from store.models import Book
+from store.serializers import BooksSerializer
 
 
 class BooksApiTestCase(APITestCase):
     def test_get(self):
-        book_1 = Book.objects.create(name='Test book 1', price=10.00)
-        book_2 = Book.objects.create(name='Test book 2', price=17.00)
-        url = reverse('book-list')
+        book_1 = Book.objects.create(name="Test book 1", price=10.00)
+        book_2 = Book.objects.create(name="Test book 2", price=17.00)
+        url = reverse("book-list")
         response = self.client.get(url)
         serial_data = BooksSerializer([book_1, book_2], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serial_data, response.data)
-
-
